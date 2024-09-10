0fa1bb32352329958241d86705a2eab7e0ad336b
Udit <23015406+udit-001@users.noreply.github.com>
2024-09-11 03:00:46 +0530

    fix: add default value for use_sample_data

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    username: str 
+    username: str
     password: str
     rapidapi_key: str
-    use_sample_data: bool
+    use_sample_data: bool = False
 
     model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
