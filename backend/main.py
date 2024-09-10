cf7718027760d00e44518607234ca4ec5e94c028
Udit <23015406+udit-001@users.noreply.github.com>
2024-09-11 03:40:07 +0530

    feat(backend): add cors support for frontend server

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ def create_app():
     origins = [
         "http://localhost",
         "http://localhost:8000",
-        "http://localhost:5173",
+        "http://localhost:3000",
     ]
 
     app.add_middleware(
